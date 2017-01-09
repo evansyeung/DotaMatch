4d4e58877995872afefb5f8b26d3f27de9d430ce
Evans Yeung <evansy0ung@yahoo.com>
2017-01-08 22:23:54 -0500

    Preference Update

--- DotaMatch/app/src/main/java/com/dotamatch/designproject/dotamatch/PreferenceActivity.java
+++ DotaMatch/app/src/main/java/com/dotamatch/designproject/dotamatch/PreferenceActivity.java
@@ -3,6 +3,7 @@ package com.dotamatch.designproject.dotamatch;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,6 +22,8 @@ public class PreferenceActivity extends AppCompatActivity implements View.OnClic
 
     private FirebaseAuth firebaseAuth;
 
+    private FirebaseUser currentUser;
+
     private EditText editTextDotaName;
     private EditText editTextMMR;
     private EditText editTextRole;
@@ -28,6 +31,10 @@ public class PreferenceActivity extends AppCompatActivity implements View.OnClic
     private RatingBar ratingBarRating;
 
     private Button buttonSave;
+    private Button buttonUpdate;
+
+    private String[] roles = {"disabler", "initiator", "jungler", "support", "durable", "nuker", "pusher", "escape"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +47,8 @@ public class PreferenceActivity extends AppCompatActivity implements View.OnClic
         //Initialize firebase authentication object
         firebaseAuth = FirebaseAuth.getInstance();
 
+        currentUser = firebaseAuth.getCurrentUser();
+
         editTextDotaName = (EditText) findViewById(R.id.editTextDotaName);
         editTextMMR = (EditText) findViewById(R.id.editTextMMR);
         editTextRole = (EditText) findViewById(R.id.editTextRole);
@@ -47,10 +56,12 @@ public class PreferenceActivity extends AppCompatActivity implements View.OnClic
         ratingBarRating = (RatingBar) findViewById(R.id.ratingBarRating);
 
         buttonSave = (Button) findViewById(R.id.buttonSave);
+        buttonUpdate = (Button) findViewById(R.id.buttonUpdate);
 
         ratingBarRating.setOnClickListener(this);
 
         buttonSave.setOnClickListener(this);
+        buttonUpdate.setOnClickListener(this);
     }
 
     private void saveUserInformation() {
@@ -62,19 +73,72 @@ public class PreferenceActivity extends AppCompatActivity implements View.OnClic
 
         User userInformation = new User(DotaName, mmr, role, rating);
 
-        //To store user into Firebase Database
-        //Get unique id of log in user
-        FirebaseUser user = firebaseAuth.getCurrentUser();
+        //Store user information into Firebase Database
+        //Get unique id of login of current user
 
-        databaseReference.child(user.getUid()).setValue(userInformation);
+        databaseReference.child(currentUser.getUid()).setValue(userInformation);
 
         Toast.makeText(this, "Information Save...", Toast.LENGTH_LONG).show();
+    }
+
+    private void updateUserInformation() {
+        String DotaName = editTextDotaName.getText().toString().trim();
+        String mmrTemp = editTextMMR.getText().toString().trim();
+        String role = editTextRole.getText().toString().trim();
+        //float rating = ratingBarRating.getRating();
+
+        boolean changesMade = false;
+        boolean bool_DotaName = TextUtils.isEmpty(DotaName);
+        boolean bool_mmrTemp = TextUtils.isEmpty(mmrTemp);
+        boolean bool_Role = TextUtils.isEmpty(role);
+
+        if(bool_DotaName && bool_mmrTemp && bool_Role) {   //No changes to attributes were made
+            Toast.makeText(this, "No changes were made", Toast.LENGTH_SHORT).show();
+            return;
+        }else if(!bool_DotaName){
+            if(DotaName.length() <= 15) {   //Check if name is within range
+                databaseReference.child(currentUser.getUid()).child("DotaName").setValue(DotaName);
+                changesMade = true;
+            }
+            else
+                Toast.makeText(this, "Invalid name, please re-enter", Toast.LENGTH_SHORT).show();
+        }else if(!bool_mmrTemp) {
+            Integer mmr = Integer.valueOf(mmrTemp);
+            if(mmr > 0 && mmr < 8000) { //Check for valid MMR
+                databaseReference.child(currentUser.getUid()).child("MMR").setValue(mmr);
+                changesMade = true;
+            }
+            else
+                Toast.makeText(this, "Invalid MMR, please re-enter MMR", Toast.LENGTH_SHORT).show();
+        }else if(!bool_Role) {  //Check for valid role
+            boolean match = false;
+            for(int i=0; i < roles.length; i++) {
+                if(role.toLowerCase() == roles[i]) {
+                    match = true;
+                }
+            }
+
+            if(match) {
+                databaseReference.child(currentUser.getUid()).child("role").setValue(role);
+                changesMade = true;
+            }
+            else
+                Toast.makeText(this, "Invalid role, please re-enter role", Toast.LENGTH_SHORT).show();
+        }
+
+       //Update message if changes were made
+       if(changesMade) {
+           Toast.makeText(this, "Updating...", Toast.LENGTH_SHORT).show();
+       }
     }
 
     @Override
     public void onClick(View view) {
         if(view == buttonSave) {
             saveUserInformation();
+        }
+        if(view == buttonUpdate) {
+            updateUserInformation();
         }
     }
 
